bb30473e9fe0972d5414e58f2b45a50dd9e6042e
Alex <alexander.mcgreevy@icloud.com>
2026-02-27 15:14:04 -0500

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moovie
+//
+//  Created by Alexander McGreevy on 2/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoovieApp.swift
+//  Moovie
+//
+//  Created by Alexander McGreevy on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MoovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
