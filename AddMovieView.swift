4062b83095afc616e39322ed404a44d76ae34681
Alex <alexander.mcgreevy@icloud.com>
2026-02-27 15:22:54 -0500

    getting starrted with layout but wifi sucks

---
+++ AddMovieView.swift
@@ -0,0 +1,18 @@
+//
+//  AddMovieView.swift
+//  Moovie
+//
+//  Created by Alexander McGreevy on 2/27/26.
+//
+
+import SwiftUI
+
+struct AddMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMovieView()
+}
---
+++ MovieDisplayView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDisplayView.swift
+//  Moovie
+//
+//  Created by Alexander McGreevy on 2/27/26.
+//
+
+import SwiftUI
+
+struct MovieDisplayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDisplayView()
+}
